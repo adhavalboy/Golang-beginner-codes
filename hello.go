e5550636dd9fd8c5944f6e72e872851b29bf5214
adhavalboy <dhaval.daiict@gmail.com>
2018-02-16 13:04:02 +0530

    first

---
+++ hello.go
@@ -0,0 +1,8 @@
+package main
+import "fmt"  // lib like in c std io ; fmt= format conatain scan,print etc..
+func main(){  // as main in c
+fmt.Println("Hello World!"); // println will add new line 
+}
+/*this is comment */
+//this is also comment 
+//for run in terminal type : go run hello.go  
