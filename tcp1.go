308c1f230f3c6f43bb3814f24d68e6a7e7cddcc0
adhavalboy <dhaval.daiict@gmail.com>
2018-02-16 18:07:27 +0530

    tcp1

---
+++ tcp1.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"net"
+)
+
+func main() {
+	conn, err := net.Dial("tcp", "golang.org:80")
+	if err != nil {
+		fmt.Println("something went wrong")
+	}
+	fmt.Fprint(conn, "GET / HTTP/1.0\r\n\r\n")
+	status, err := bufio.NewReader(conn).ReadString('\n')
+	fmt.Println(status)
+}
