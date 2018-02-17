562a3ca21fba93bd48175d93c36b3c95ebcb823a
adhavalboy <dhaval.daiict@gmail.com>
2018-02-17 16:44:35 +0530

    little more

---
+++ 2.variable.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	var num int64 //64 bit other are int-4bit, int8, int16, int32
+	var str string
+	num = 22
+	str = " Hola!"
+	//you can direct assign a value with out declare as below
+	x := 12
+	fmt.Println(num)
+	fmt.Println(x)
+	fmt.Println(str)
+
+}
